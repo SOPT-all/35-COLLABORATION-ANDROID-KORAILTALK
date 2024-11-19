3113c72714eece36bc072770aa225f49056a0e4e
SYAAINN <alswo08319@gmail.com>
2024-11-19 11:10:29 +0900

    [setting/#1] presentation layer 를 세팅합니다.

---
+++ app/src/main/java/com/sopt/korailtalk/presentation/ui/payment/PaymentScreen.kt
@@ -0,0 +1,18 @@
+package com.sopt.korailtalk.presentation.ui.payment
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun PaymentScreen() {
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(text = "결제하기")
+    }
+}
