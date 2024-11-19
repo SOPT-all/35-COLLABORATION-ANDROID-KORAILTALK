0a60876ffa167b09f722dac81a43b477b70d8ecf
SYAAINN <alswo08319@gmail.com>
2024-11-19 11:23:26 +0900

    [chore/#1] MainActivity ui를 수정합니다.

--- app/src/main/java/com/sopt/korailtalk/presentation/ui/main/MainActivity.kt
+++ app/src/main/java/com/sopt/korailtalk/presentation/ui/main/MainActivity.kt
@@ -4,11 +4,13 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.sopt.korailtalk.ui.theme._35COLLAVORATIONANDROIDKORAILTALKTheme
@@ -34,10 +36,15 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = "$name!",
+            modifier = modifier
+        )
+    }
 }
 
 @Preview(showBackground = true)
