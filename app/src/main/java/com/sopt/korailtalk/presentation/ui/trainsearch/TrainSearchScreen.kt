96d0515f7152f32699bacfabc48c48a4c8a30064
gitsuhyun <suhyun10201@naver.com>
2024-11-21 15:19:14 +0900

    [feat/#8] KorailChip 공통 컴포넌트 제작

--- app/src/main/java/com/sopt/korailtalk/presentation/ui/trainsearch/TrainSearchScreen.kt
+++ app/src/main/java/com/sopt/korailtalk/presentation/ui/trainsearch/TrainSearchScreen.kt
@@ -2,10 +2,12 @@ package com.sopt.korailtalk.presentation.ui.trainsearch
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.sopt.korailtalk.presentation.ui.trainsearch.component.KorailChip
+import com.sopt.korailtalk.ui.theme.COLLAVORATIONANDROIDKORAILTALKTheme
 
 @Composable
 fun TrainSearchScreen() {
@@ -13,6 +15,16 @@ fun TrainSearchScreen() {
         modifier = Modifier.fillMaxSize(),
         contentAlignment = Alignment.Center
     ) {
-        Text(text = "열차 조회")
+        KorailChip(
+            text = "KTX 009",
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    COLLAVORATIONANDROIDKORAILTALKTheme {
+        TrainSearchScreen()
     }
 }
---
+++ app/src/main/java/com/sopt/korailtalk/presentation/ui/trainsearch/component/KorailChip.kt
@@ -0,0 +1,37 @@
+package com.sopt.korailtalk.presentation.ui.trainsearch.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.sopt.korailtalk.ui.theme.KorailTalkTheme
+
+@Composable
+fun KorailChip(
+    text: String,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        horizontalArrangement = Arrangement.spacedBy(10.dp, Alignment.CenterHorizontally),
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = modifier
+            .width(106.dp)
+            .background(
+                color = KorailTalkTheme.colors.blue02,
+                shape = RoundedCornerShape(size = 10.dp)
+            )
+            .padding(horizontal = 7.dp, vertical = 3.dp)
+    ) {
+        Text(
+            text = text,
+            style = KorailTalkTheme.typography.caption2.copy(color = KorailTalkTheme.colors.white)
+        )
+    }
+}
