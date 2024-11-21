fd1c252fef4b6e9cf9b123463e54ac949aff59cf
SYAAINN <alswo08319@gmail.com>
2024-11-22 02:04:09 +0900

    [feat/#7] PaymentRadioButtonOption 을 생성하고, showIf 확장함수를 추가합니다.

---
+++ app/src/main/java/com/sopt/korailtalk/presentation/ui/payment/component/PaymentRadioButtonOption.kt
@@ -0,0 +1,118 @@
+package com.sopt.korailtalk.presentation.ui.payment.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.sopt.korailtalk.presentation.util.clickableWithoutRipple
+import com.sopt.korailtalk.ui.theme.COLLAVORATIONANDROIDKORAILTALKTheme
+import com.sopt.korailtalk.ui.theme.KorailTalkTheme
+
+@Composable
+fun PaymentRadioButtonOption(
+    optionName: String,
+    selected: Boolean,
+    onClick: () -> Unit,
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clickableWithoutRipple(onClick = onClick),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Box(
+            modifier = Modifier
+                .size(20.dp)
+                .border(
+                    width = 1.dp,
+                    color = KorailTalkTheme.colors.grey200,
+                    shape = CircleShape
+                )
+                .clickable(onClick = onClick),
+            contentAlignment = Alignment.Center
+        ) {
+            if (selected) {
+                Box(
+                    modifier = Modifier
+                        .size(12.dp)
+                        .background(color = KorailTalkTheme.colors.blue04, shape = CircleShape)
+                )
+            }
+        }
+        Spacer(modifier = Modifier.width(6.dp))
+        Text(
+            text = optionName,
+            modifier = Modifier.padding(vertical = 6.dp),
+            color = KorailTalkTheme.colors.grey800,
+            style = KorailTalkTheme.typography.body1
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ShowPaymentRadioButtonOption() {
+    COLLAVORATIONANDROIDKORAILTALKTheme {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp)
+        ) {
+            Spacer(modifier = Modifier.height(16.dp))
+
+            PaymentRadioButtonOption(
+                optionName = "KTX 마일리지",
+                selected = false,
+                onClick = { }
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            PaymentRadioButtonOption(
+                optionName = "할인쿠폰",
+                selected = false,
+                onClick = { }
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            PaymentRadioButtonOption(
+                optionName = "포인트 사용",
+                selected = false,
+                onClick = { }
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            PaymentRadioButtonOption(
+                optionName = "간편결제",
+                selected = false,
+                onClick = { }
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            PaymentRadioButtonOption(
+                optionName = "카드결제",
+                selected = false,
+                onClick = { }
+            )
+        }
+    }
+}
--- app/src/main/java/com/sopt/korailtalk/presentation/util/ModifierExt.kt
+++ app/src/main/java/com/sopt/korailtalk/presentation/util/ModifierExt.kt
@@ -4,6 +4,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
@@ -38,4 +39,8 @@ fun Modifier.clickableWithoutRipple(
     ) {
         onClick()
     }
+}
+
+fun Modifier.showIf(condition: Boolean): Modifier {
+    return if (condition) this else Modifier.size(0.dp)
 }
