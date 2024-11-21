3c1187f1243ad9e8c42c94d0f5179cb5329903b5
TaejungKim <92737123+imtaejugkim@users.noreply.github.com>
2024-11-22 04:17:21 +0900

    [feat/#9] SeatMapSeatSelector 개별 컴포넌트 작성
    - 좌석 컴포넌트 구현
    - 이미지 리소스 추가

--- app/src/main/java/com/sopt/korailtalk/presentation/ui/seatmap/component/SeatMapSeatSelector.kt
+++ app/src/main/java/com/sopt/korailtalk/presentation/ui/seatmap/component/SeatMapSeatSelector.kt
@@ -1,4 +1,57 @@
 package com.sopt.korailtalk.presentation.ui.seatmap.component
 
-class SeatMapSeatSelector {
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.material3.Text
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.sopt.korailtalk.R
+
+@Composable
+fun SeatMapSeatSelector(
+    seatId: Long,
+    seatName: String,
+    direction: Boolean,
+    isSold: Boolean,
+    isSelected: Boolean,
+    onSelectionChange: (Long?) -> Unit
+) {
+    val image = when {
+        isSold -> if (direction) R.drawable.icn_seatmap_forward_inactive else R.drawable.icn_seatmap_backward_inactive
+        isSelected -> if (direction) R.drawable.icn_seatmap_forward_selected else R.drawable.icn_seatmap_backward_selected
+        else -> if (direction) R.drawable.icn_seatmap_forward_activate else R.drawable.icn_seatmap_backward_activate
+    }
+
+    Box(
+        modifier = Modifier
+            .clickable(enabled = !isSold) {
+                onSelectionChange(if (isSelected) null else seatId)
+            }
+    ){
+        Image(painter = painterResource(id = image),
+            contentDescription = "좌석 이미지")
+        Text(text = seatName,
+            modifier = Modifier.align(Alignment.Center))
+    }
+}
+
+@Preview
+@Composable
+fun SeatMapSeatSelectorPreview() {
+    // 클릭 시 해당 좌석 아이디를 반환, 아무 좌석이 선택 안된 경우는 null
+    var selectedSeatId by remember { mutableStateOf<Long?>(null) }
+    SeatMapSeatSelector(
+        seatId = 1,
+        seatName = "1A",
+        direction = false,
+        isSold = false,
+        isSelected = selectedSeatId?.toInt() == 1,
+        onSelectionChange = { newSeatId ->
+            selectedSeatId = newSeatId
+        }
+    )
 }
