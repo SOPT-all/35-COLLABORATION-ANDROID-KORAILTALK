4c9873227e6ebf5cf806ecc1fcc52e51204968ae
gitsuhyun <suhyun10201@naver.com>
2024-11-21 17:06:16 +0900

    [feat/#8] MyTicketInfoItem 개별 컴포넌트 제작

--- app/src/main/java/com/sopt/korailtalk/presentation/ui/myticket/MyTicketScreen.kt
+++ app/src/main/java/com/sopt/korailtalk/presentation/ui/myticket/MyTicketScreen.kt
@@ -1,11 +1,17 @@
 package com.sopt.korailtalk.presentation.ui.myticket
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.sopt.korailtalk.ui.theme.COLLAVORATIONANDROIDKORAILTALKTheme
+import com.sopt.korailtalk.ui.theme.KorailTalkTheme
 
 @Composable
 fun MyTicketScreen() {
@@ -13,6 +19,34 @@ fun MyTicketScreen() {
         modifier = Modifier.fillMaxSize(),
         contentAlignment = Alignment.Center
     ) {
-        Text(text = "나의 티켓")
+    }
+}
+
+@Composable
+fun MyTicketInfoItem(
+    title: String,
+    content: @Composable () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        verticalArrangement = Arrangement.spacedBy(0.dp, Alignment.Top),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
+    ) {
+        Text(
+            text = title,
+            style = KorailTalkTheme.typography.caption1.copy(
+                color = KorailTalkTheme.colors.black
+            )
+        )
+        content()
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    COLLAVORATIONANDROIDKORAILTALKTheme {
+        MyTicketScreen()
     }
 }
