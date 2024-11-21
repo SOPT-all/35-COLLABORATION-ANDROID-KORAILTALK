cef0c65a68043ac30ccfaf0358e001240076371f
gitsuhyun <suhyun10201@naver.com>
2024-11-21 17:58:45 +0900

    [feat/#8] MyTicketAdditionalService 개별 컴포넌트 제작

--- app/src/main/java/com/sopt/korailtalk/presentation/ui/myticket/MyTicketScreen.kt
+++ app/src/main/java/com/sopt/korailtalk/presentation/ui/myticket/MyTicketScreen.kt
@@ -1,5 +1,6 @@
 package com.sopt.korailtalk.presentation.ui.myticket
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -7,15 +8,18 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.sopt.korailtalk.R
 import com.sopt.korailtalk.ui.theme.COLLAVORATIONANDROIDKORAILTALKTheme
 import com.sopt.korailtalk.ui.theme.KorailTalkTheme
 
@@ -28,12 +32,11 @@ fun MyTicketScreen() {
         contentAlignment = Alignment.Center
     ) {
         Column {
-            Row (
+            Row(
                 horizontalArrangement = Arrangement.spacedBy(4.dp, Alignment.Start),
                 verticalAlignment = Alignment.CenterVertically,
-            ){
-                myTicketButtonText.forEach {
-                    text ->
+            ) {
+                myTicketButtonText.forEach { text ->
                     MyTicketButton(
                         text = text,
                         onClick = {},
@@ -42,13 +45,87 @@ fun MyTicketScreen() {
                     )
                 }
             }
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
+                for (item in AdditionalServiceItem.entries) {
+                    MyTicketAdditionalService(
+                        image = item.image,
+                        text = item.text,
+                        textColor = item.textColor,
+                    )
+                }
+            }
         }
 
     }
 }
 
+enum class AdditionalServiceItem(
+    val image: @Composable () -> Unit,
+    val text: String,
+    val textColor: @Composable () -> Color
+) {
+    REPORT(
+        image = {
+            Image(
+                painter = painterResource(R.drawable.ic_ticket_report),
+                contentDescription = "철도범죄신고"
+            )
+        },
+        text = "철도범죄신고",
+        textColor = { KorailTalkTheme.colors.grey500 }
+    ),
+    HELP(
+        image = {
+            Image(
+                painter = painterResource(R.drawable.ic_ticket_help),
+                contentDescription = "승하차 도우미 신청"
+            )
+        },
+        text = "승하차 도우미 신청",
+        textColor = { KorailTalkTheme.colors.grey300 }
+    ),
+    SMS(
+        image = {
+            Image(
+                painter = painterResource(R.drawable.ic_ticket_sms),
+                contentDescription = "보호자 안심 SMS"
+            )
+        },
+        text = "보호자 안심 SMS",
+        textColor = { KorailTalkTheme.colors.grey300 }
+    )
+}
+
 @Composable
-private fun MyTicketButton(
+fun MyTicketAdditionalService(
+    image: @Composable () -> Unit,
+    text: String,
+    textColor: @Composable () -> Color,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        verticalArrangement = Arrangement.spacedBy(8.dp, Alignment.Top),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
+            .padding(top = 4.dp, bottom = 8.dp)
+    ) {
+        image()
+        Text(
+            text = text,
+            style = KorailTalkTheme.typography.caption1.copy(
+                color = textColor()
+            ),
+        )
+    }
+}
+
+@Composable
+fun MyTicketButton(
     text: String,
     onClick: () -> Unit,
     modifier: Modifier = Modifier
