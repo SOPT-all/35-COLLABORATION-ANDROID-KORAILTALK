3e4ac987bcc0248546bd8e1d4c517d1cccb97f9d
gitsuhyun <suhyun10201@naver.com>
2024-11-21 16:29:25 +0900

    [feat/#8] SearchTrainDateChip 개별 컴포넌트 제작

--- app/src/main/java/com/sopt/korailtalk/presentation/ui/trainsearch/TrainSearchScreen.kt
+++ app/src/main/java/com/sopt/korailtalk/presentation/ui/trainsearch/TrainSearchScreen.kt
@@ -2,6 +2,7 @@ package com.sopt.korailtalk.presentation.ui.trainsearch
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -10,8 +11,13 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
@@ -41,7 +47,64 @@ fun TrainSearchScreen() {
                 date = "2024.11.16 (토)"
             )
 
+            val chipList = listOf("11:08", "11:18", "11:28", "11:38")
+
+            SearchTrainDateChipGroup(
+                chipList = chipList
+            )
         }
+    }
+}
+
+@Composable
+fun SearchTrainDateChipGroup(
+    chipList: List<String>,
+    modifier: Modifier = Modifier
+) {
+    var selectedTime by remember { mutableStateOf<String?>(null) }
+
+    Row(
+        horizontalArrangement = Arrangement.spacedBy(10.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        chipList.forEach { chipTime ->
+            SearchTrainDateChip(
+                date = chipTime,
+                isSelected = selectedTime == chipTime,
+                onSelectedChange = {
+                    selectedTime = if (selectedTime == chipTime) null else chipTime
+                }
+            )
+        }
+    }
+}
+
+@Composable
+fun SearchTrainDateChip(
+    date: String,
+    modifier: Modifier = Modifier,
+    isSelected: Boolean = false,
+    onSelectedChange: (Boolean) -> Unit = {},
+) {
+    Row(
+        horizontalArrangement = Arrangement.spacedBy(10.dp, Alignment.CenterHorizontally),
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .background(
+                color = if (isSelected) KorailTalkTheme.colors.blue04 else KorailTalkTheme.colors.grey25,
+                shape = RoundedCornerShape(size = 17.dp)
+            )
+            .padding(horizontal = 16.dp, vertical = 6.dp)
+            .clickable {
+                onSelectedChange(isSelected)
+            }
+    ) {
+        Text(
+            text = date,
+            style = KorailTalkTheme.typography.body3.copy(
+                color = if (isSelected) KorailTalkTheme.colors.white else KorailTalkTheme.colors.grey500
+            )
+        )
     }
 }
 
