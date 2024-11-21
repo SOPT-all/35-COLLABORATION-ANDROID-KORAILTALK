bdd93b996d558ed89a0adaf5a79185180be12709
gitsuhyun <suhyun10201@naver.com>
2024-11-21 17:29:07 +0900

    [feat/#8] MyTicketButton 개별 컴포넌트 제작

--- app/src/main/java/com/sopt/korailtalk/presentation/ui/myticket/MyTicketScreen.kt
+++ app/src/main/java/com/sopt/korailtalk/presentation/ui/myticket/MyTicketScreen.kt
@@ -1,13 +1,19 @@
 package com.sopt.korailtalk.presentation.ui.myticket
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.sopt.korailtalk.ui.theme.COLLAVORATIONANDROIDKORAILTALKTheme
@@ -15,10 +21,56 @@ import com.sopt.korailtalk.ui.theme.KorailTalkTheme
 
 @Composable
 fun MyTicketScreen() {
+    val myTicketButtonText = listOf("승차권 전달", "예매 취소", "예매 변경")
+
     Box(
         modifier = Modifier.fillMaxSize(),
         contentAlignment = Alignment.Center
     ) {
+        Column {
+            Row (
+                horizontalArrangement = Arrangement.spacedBy(4.dp, Alignment.Start),
+                verticalAlignment = Alignment.CenterVertically,
+            ){
+                myTicketButtonText.forEach {
+                    text ->
+                    MyTicketButton(
+                        text = text,
+                        onClick = {},
+                        modifier = Modifier
+                            .weight(1f)
+                    )
+                }
+            }
+        }
+
+    }
+}
+
+@Composable
+private fun MyTicketButton(
+    text: String,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Box(
+        modifier = modifier
+            .background(
+                color = KorailTalkTheme.colors.blue07,
+                shape = RoundedCornerShape(size = 8.dp)
+            )
+            .clickable {
+                onClick()
+            }
+            .padding(vertical = 17.dp),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = text,
+            style = KorailTalkTheme.typography.title3.copy(
+                color = KorailTalkTheme.colors.blue01
+            ),
+        )
     }
 }
 
