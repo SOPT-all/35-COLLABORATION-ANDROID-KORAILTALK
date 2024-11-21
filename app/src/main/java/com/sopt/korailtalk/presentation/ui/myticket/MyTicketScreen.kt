3f73b4e88ca4a6db3830c7033a20a9bd36e1d84d
gitsuhyun <suhyun10201@naver.com>
2024-11-21 18:14:27 +0900

    [feat/#8] MyTicketTabBar 개별 컴포넌트 제작

--- app/src/main/java/com/sopt/korailtalk/presentation/ui/myticket/MyTicketScreen.kt
+++ app/src/main/java/com/sopt/korailtalk/presentation/ui/myticket/MyTicketScreen.kt
@@ -11,8 +11,16 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Tab
+import androidx.compose.material3.TabRow
+import androidx.compose.material3.TabRowDefaults
+import androidx.compose.material3.TabRowDefaults.tabIndicatorOffset
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -26,12 +34,15 @@ import com.sopt.korailtalk.ui.theme.KorailTalkTheme
 @Composable
 fun MyTicketScreen() {
     val myTicketButtonText = listOf("승차권 전달", "예매 취소", "예매 변경")
+    val myTicketTabText = listOf("승차권 (1)", "이용권", "정기권 · 패스")
 
     Box(
         modifier = Modifier.fillMaxSize(),
         contentAlignment = Alignment.Center
     ) {
         Column {
+            MyTicketTabBar(myTicketTabText = myTicketTabText)
+
             Row(
                 horizontalArrangement = Arrangement.spacedBy(4.dp, Alignment.Start),
                 verticalAlignment = Alignment.CenterVertically,
@@ -59,6 +70,42 @@ fun MyTicketScreen() {
                     )
                 }
             }
+        }
+
+    }
+}
+
+@Composable
+fun MyTicketTabBar(
+    myTicketTabText: List<String>,
+    modifier: Modifier = Modifier
+) {
+    var selectedTabIndex by remember { mutableIntStateOf(0) }
+
+    TabRow(
+        selectedTabIndex = selectedTabIndex,
+        containerColor = KorailTalkTheme.colors.white,
+        contentColor = KorailTalkTheme.colors.white,
+        indicator = { tabPositions ->
+            TabRowDefaults.SecondaryIndicator(
+                modifier = Modifier.tabIndicatorOffset(tabPositions[selectedTabIndex]),
+                color = KorailTalkTheme.colors.blue01
+            )
+        },
+        divider = {}
+    ) {
+        myTicketTabText.forEachIndexed { index, tab ->
+            Tab(
+                selected = selectedTabIndex == index,
+                onClick = { selectedTabIndex = index },
+                text = {
+                    Text(
+                        text = tab,
+                        style = KorailTalkTheme.typography.title1,
+                        color = if (selectedTabIndex == index) KorailTalkTheme.colors.blue01 else KorailTalkTheme.colors.grey400,
+                    )
+                }
+            )
         }
 
     }
