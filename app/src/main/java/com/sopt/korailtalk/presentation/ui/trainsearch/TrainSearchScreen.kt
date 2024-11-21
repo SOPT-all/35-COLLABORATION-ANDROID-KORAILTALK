d1e037ea98b11c2f72bea481a7dd7d6a4fbb2ebe
gitsuhyun <suhyun10201@naver.com>
2024-11-21 15:35:06 +0900

    [feat/#8] SearchDepartureToArrival 개별 컴포넌트 제작

--- app/src/main/java/com/sopt/korailtalk/presentation/ui/trainsearch/TrainSearchScreen.kt
+++ app/src/main/java/com/sopt/korailtalk/presentation/ui/trainsearch/TrainSearchScreen.kt
@@ -1,13 +1,22 @@
 package com.sopt.korailtalk.presentation.ui.trainsearch
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.sopt.korailtalk.R
 import com.sopt.korailtalk.presentation.ui.trainsearch.component.KorailChip
 import com.sopt.korailtalk.ui.theme.COLLAVORATIONANDROIDKORAILTALKTheme
+import com.sopt.korailtalk.ui.theme.KorailTalkTheme
 
 @Composable
 fun TrainSearchScreen() {
@@ -15,8 +24,39 @@ fun TrainSearchScreen() {
         modifier = Modifier.fillMaxSize(),
         contentAlignment = Alignment.Center
     ) {
-        KorailChip(
-            text = "KTX 009",
+        Column {
+            KorailChip(
+                text = "KTX 009",
+            )
+            SearchDepartureToArrival(
+                departureTime = "05:13",
+                arrivalTime = "07:13"
+            )
+        }
+    }
+}
+
+@Composable
+private fun SearchDepartureToArrival(
+    departureTime: String,
+    arrivalTime: String,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        horizontalArrangement = Arrangement.spacedBy(5.dp, Alignment.Start),
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        Text(
+            text = departureTime,
+            style = KorailTalkTheme.typography.title2.copy(color = KorailTalkTheme.colors.black)
+        )
+        Image(
+            painter = painterResource(R.drawable.icn_trainsearch_arrow_right),
+            contentDescription = "화살표"
+        )
+        Text(
+            text = arrivalTime,
+            style = KorailTalkTheme.typography.title2.copy(color = KorailTalkTheme.colors.black)
         )
     }
 }
