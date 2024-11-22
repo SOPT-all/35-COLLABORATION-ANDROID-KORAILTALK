6d0c80187d135a7f1e9ea6d258e79d3728c438a0
gitsuhyun <suhyun10201@naver.com>
2024-11-22 15:05:52 +0900

    [feat/#8] MyTicketInfoItem 개별 컴포넌트 제작

--- app/src/main/java/com/sopt/korailtalk/presentation/ui/myticket/MyTicketScreen.kt
+++ app/src/main/java/com/sopt/korailtalk/presentation/ui/myticket/MyTicketScreen.kt
@@ -25,6 +25,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.sopt.korailtalk.R
@@ -42,6 +43,11 @@ fun MyTicketScreen() {
     ) {
         Column {
             MyTicketTabBar(myTicketTabText = myTicketTabText)
+
+            MyTicketInfomation(
+                coachesNumber = 4,
+                seatName = "16A"
+            )
 
             Row(
                 horizontalArrangement = Arrangement.spacedBy(4.dp, Alignment.Start),
@@ -72,6 +78,94 @@ fun MyTicketScreen() {
             }
         }
 
+    }
+}
+
+@Composable
+private fun MyTicketInfomation(
+    coachesNumber: Int,
+    seatName: String,
+    modifier: Modifier = Modifier
+) {
+    Row(
+        modifier = modifier
+            .fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        MyTicketInfoItem(
+            title = "승차권",
+            content = {
+                Column(
+                    verticalArrangement = Arrangement.spacedBy(2.dp, Alignment.CenterVertically),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                ) {
+                    Image(
+                        painter = painterResource(R.drawable.ic_ticket_refresh_l),
+                        contentDescription = "리프레시"
+                    )
+                    Text(
+                        text = "15분전에\n표시됩니다",
+                        style = KorailTalkTheme.typography.caption1.copy(
+                            color = KorailTalkTheme.colors.blue02
+                        ),
+                        textAlign = TextAlign.Center
+                    )
+                }
+            },
+        )
+        MyTicketInfoItem(
+            title = "호차번호",
+            content = {
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                ) {
+                    Text(
+                        text = coachesNumber.toString(),
+                        style = KorailTalkTheme.typography.head2.copy(
+                            color = KorailTalkTheme.colors.blue02
+                        )
+                    )
+                    Text(
+                        text = "호차",
+                        style = KorailTalkTheme.typography.body1.copy(
+                            color = KorailTalkTheme.colors.blue02
+                        )
+                    )
+                }
+            },
+        )
+        MyTicketInfoItem(
+            title = "좌석번호",
+            content = {
+                Column(
+                    verticalArrangement = Arrangement.spacedBy(2.dp, Alignment.CenterVertically),
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                ) {
+                    Text(
+                        text = seatName,
+                        style = KorailTalkTheme.typography.head2.copy(
+                            color = KorailTalkTheme.colors.blue02
+                        )
+                    )
+                    Text(
+                        text = "창측",
+                        style = KorailTalkTheme.typography.body2.copy(
+                            color = KorailTalkTheme.colors.blue02
+                        )
+                    )
+                }
+            },
+        )
+        MyTicketInfoItem(
+            title = "운임영수증",
+            content = {
+                Image(
+                    painter = painterResource(R.drawable.img_ticket_qr),
+                    contentDescription = "영수증"
+                )
+            },
+        )
     }
 }
 
@@ -205,7 +299,7 @@ fun MyTicketInfoItem(
     modifier: Modifier = Modifier
 ) {
     Column(
-        verticalArrangement = Arrangement.spacedBy(0.dp, Alignment.Top),
+        verticalArrangement = Arrangement.SpaceEvenly,
         horizontalAlignment = Alignment.CenterHorizontally,
         modifier = modifier
     ) {
@@ -213,7 +307,7 @@ fun MyTicketInfoItem(
             text = title,
             style = KorailTalkTheme.typography.caption1.copy(
                 color = KorailTalkTheme.colors.black
-            )
+            ),
         )
         content()
     }
