dd0a89f707e5de316f5574db5e54e6eb550e14db
TaejungKim <92737123+imtaejugkim@users.noreply.github.com>
2024-11-22 13:00:23 +0900

    [del/#9] 사용하지 않은 import 구문 삭제

--- app/src/main/java/com/sopt/korailtalk/presentation/ui/KorailWayInfoWithTime.kt
+++ app/src/main/java/com/sopt/korailtalk/presentation/ui/KorailWayInfoWithTime.kt
@@ -4,9 +4,7 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
