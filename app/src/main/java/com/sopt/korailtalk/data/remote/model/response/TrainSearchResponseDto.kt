ae1b38754172684ebd2222ca507475157bc3bd45
gitsuhyun <suhyun10201@naver.com>
2024-11-25 20:37:48 +0900

    [feat/#14] SearchDetailBottomSheet 개별 컴포넌트 제작

---
+++ app/src/main/java/com/sopt/korailtalk/data/remote/model/response/TrainSearchResponseDto.kt
@@ -0,0 +1,39 @@
+package com.sopt.korailtalk.data.remote.model.response
+
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class TrainSearchResponseDto(
+    @SerialName("data")
+    val data: Data
+)
+
+@Serializable
+data class Data(
+    @SerialName("timetables")
+    val timetables: List<Timetable?>
+)
+
+@Serializable
+data class Timetable(
+    @SerialName("timetableId")
+    val timetableId: Long?,
+    @SerialName("trainName")
+    val trainName: String,
+    @SerialName("departureTime")
+    val departureTime: String,
+    @SerialName("arrivalTime")
+    val arrivalTime: String,
+    @SerialName("standardPrice")
+    val standardPrice: Int,
+    @SerialName("premiumPrice")
+    val premiumPrice: Int,
+    @SerialName("isStandardSold")
+    val isStandardSold: Boolean,
+    @SerialName("isPremiumSold")
+    val isPremiumSold: Boolean,
+    @SerialName("travelTime")
+    val travelTime: Int,
+)
