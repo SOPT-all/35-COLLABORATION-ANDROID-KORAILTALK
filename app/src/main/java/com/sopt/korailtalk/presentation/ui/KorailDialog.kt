4edb5b081f87ad5cbaf087248d7102732542c785
TaejungKim <92737123+imtaejugkim@users.noreply.github.com>
2024-11-27 03:04:45 +0900

    [chore/#12] seatMap, trainCheck string 추출

--- app/src/main/java/com/sopt/korailtalk/presentation/ui/KorailDialog.kt
+++ app/src/main/java/com/sopt/korailtalk/presentation/ui/KorailDialog.kt
@@ -19,6 +19,7 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
+import androidx.compose.ui.window.DialogProperties
 import com.sopt.korailtalk.R
 import com.sopt.korailtalk.ui.theme.KorailTalkTheme
 import com.sopt.korailtalk.ui.theme.KorailTalkTheme.typography
@@ -31,9 +32,11 @@ fun KorailDialog(
 ) {
     Dialog(
         onDismissRequest = {},
+        properties = DialogProperties(usePlatformDefaultWidth = false)
     ) {
         Column(modifier = Modifier
             .fillMaxWidth()
+            .padding(horizontal = 14.dp)
             .clip(RoundedCornerShape(12.dp))
             .background(color = KorailTalkTheme.colors.white)
         ){
