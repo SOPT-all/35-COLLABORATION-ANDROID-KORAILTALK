d744e7ed8f683b267ca760612eed77b0a23415de
TaejungKim <92737123+imtaejugkim@users.noreply.github.com>
2024-11-29 02:25:56 +0900

    [feat/#23] 좌석 선택 뷰 state에 따른 데이터 주입 구현

--- app/src/main/java/com/sopt/korailtalk/presentation/ui/seatmap/SeatMapViewModel.kt
+++ app/src/main/java/com/sopt/korailtalk/presentation/ui/seatmap/SeatMapViewModel.kt
@@ -1,47 +1,59 @@
 package com.sopt.korailtalk.presentation.ui.seatmap
 
+import android.util.Log
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.sopt.korailtalk.domain.model.SeatData
+import com.sopt.korailtalk.domain.model.SeatMapData
+import com.sopt.korailtalk.domain.repository.SeatsRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 import kotlin.random.Random
 
+@HiltViewModel
 class SeatMapViewModel @Inject constructor(
+    private val seatsRepository: SeatsRepository
 )  : ViewModel() {
 
-    private var _seatsMapData = mutableStateOf<List<SeatMapData>>(emptyList())
-    val seatsMapData: List<SeatMapData>
-        get() = _seatsMapData.value
     var selectedCoachId = mutableStateOf<Long?>(null)
     var selectedSeatId = mutableStateOf<Long?>(null)
     var showDialog = mutableStateOf(false)
 
-    init {
-        loadSeatsMapData()
-        loadInitCoachId()
-    }
+    private var _leftSeatsState = MutableStateFlow<LeftSeatsState>(LeftSeatsState.Idle)
+    val leftSeatsState: StateFlow<LeftSeatsState> = _leftSeatsState
 
-    private fun loadSeatsMapData() {
-        val seats = arrayListOf<SeatData>()
-        for (i in 1..60) {
-            val seatId = i.toLong()
-            val seatName = "${(i - 1) / 4 + 1}${charArrayOf('A', 'B', 'C', 'D')[(i - 1) % 4]}"
-            val direction = i <= 30
-            val isSold = Random.nextBoolean()  // 무작위로 판매 상태 결정
-
-            seats.add(SeatData(seatId, seatName, direction, isSold))
-        }
-        _seatsMapData.value = listOf(
-            SeatMapData(1L, 40, seats),
-            SeatMapData(2L, 40, arrayListOf()),
-            SeatMapData(3L, 40, arrayListOf()),
-            SeatMapData(4L, 40, arrayListOf()),
-            SeatMapData(5L, 40, arrayListOf())
-        )
-    }
+    private val _seatsMapData = MutableStateFlow<ArrayList<SeatMapData>>(arrayListOf())
+    val seatsMapData: StateFlow<ArrayList<SeatMapData>>
+        get() = _seatsMapData
 
     private fun loadInitCoachId() {
-        if (seatsMapData.isNotEmpty()) {
-            selectedCoachId.value = seatsMapData.first().coachId
+        viewModelScope.launch {
+            if (seatsMapData.value.isNotEmpty()) {
+                selectedCoachId.value = seatsMapData.value.first().coachId
+            }
+        }
+    }
+
+    fun getLeftSeats(userId: Long, timetableId: Long) {
+        _leftSeatsState.value = LeftSeatsState.Loading
+        viewModelScope.launch {
+            val result = seatsRepository.getLeftSeats(userId = userId, timetableId = timetableId)
+            result.fold(
+                onSuccess = { leftSeats ->
+                    _seatsMapData.value = leftSeats.coaches
+                    _leftSeatsState.value = LeftSeatsState.Success(leftSeats)
+                    loadInitCoachId()
+                    Log.d("SeatMapViewModel", "${_seatsMapData.value}")
+                },
+                onFailure = { throwable ->
+                    _leftSeatsState.value = LeftSeatsState.Failure(throwable.message ?: "알 수 없는 오류")
+                    Log.e("SeatMapViewModel", "로드 실패: ${throwable.message}")
+                }
+            )
         }
     }
 
