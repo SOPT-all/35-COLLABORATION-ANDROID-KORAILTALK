f434c6e85c0cdffc3ca660327d060f22ae9b7f4e
SYAAINN <alswo08319@gmail.com>
2024-11-28 15:27:07 +0900

    [feat/#15] 의존성 주입에 대한 파일들을 정의합니다.

--- app/src/main/java/com/sopt/korailtalk/di/NetworkModule.kt
+++ app/src/main/java/com/sopt/korailtalk/di/NetworkModule.kt
@@ -2,21 +2,50 @@ package com.sopt.korailtalk.di
 
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import com.sopt.korailtalk.BuildConfig
+import com.sopt.korailtalk.BuildConfig.DEBUG
+import com.sopt.korailtalk.di.qualifier.Auth
 import com.sopt.korailtalk.di.qualifier.KorailTalk
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.json.Json
+import okhttp3.Interceptor
 import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
+import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object NetworkModule {
+    @Provides
+    @Singleton
+    fun providesOkHttpClient(
+        loggingInterceptor: HttpLoggingInterceptor,
+        @Auth authInterceptor: Interceptor
+    ): OkHttpClient =
+        OkHttpClient.Builder().apply {
+            connectTimeout(10, TimeUnit.SECONDS)
+            writeTimeout(10, TimeUnit.SECONDS)
+            readTimeout(10, TimeUnit.SECONDS)
+            addInterceptor(authInterceptor)
+            if (DEBUG) addInterceptor(loggingInterceptor)
+        }.build()
+
+    @Provides
+    @Singleton
+    fun providesJson(): Json =
+        Json {
+            isLenient = true
+            prettyPrint = true
+            explicitNulls = false
+            ignoreUnknownKeys = true
+        }
+
     @Provides
     @Singleton
     fun provideLoggingInterceptor(): HttpLoggingInterceptor = HttpLoggingInterceptor().apply {
@@ -25,20 +54,38 @@ object NetworkModule {
 
     @Provides
     @Singleton
-    @KorailTalk
-    fun provideKorailTalkBaseUrl(): String = BuildConfig.BASE_URL
+    @Auth
+    fun provideAuthInterceptor(): Interceptor = Interceptor { chain ->
+        val request = chain.request().newBuilder()
+            .addHeader("Authorization", "Bearer <Your_Token_Here>")
+            .build()
+        chain.proceed(request)
+    }
 
     @Provides
     @Singleton
     fun provideRetrofit(
         okHttpClient: OkHttpClient,
-        baseUrl: String
+        json: Json
     ): Retrofit {
-        val json = Json { ignoreUnknownKeys = true }
         return Retrofit.Builder()
-            .addConverterFactory(json.asConverterFactory("application/json".toMediaType()))
-            .baseUrl(baseUrl)
+            .baseUrl(BuildConfig.BASE_URL)
             .client(okHttpClient)
+            .addConverterFactory(json.asConverterFactory("application/json".toMediaType()))
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    @KorailTalk
+    fun provideKorailTalkRetrofit(
+        okHttpClient: OkHttpClient,
+        json: Json
+    ): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BuildConfig.BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(json.asConverterFactory("application/json".toMediaType()))
             .build()
     }
 }
