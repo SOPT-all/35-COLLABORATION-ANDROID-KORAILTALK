7d129667052d96eb7fa8262b544ad24e9a6baa90
gitsuhyun <suhyun10201@naver.com>
2024-11-29 15:47:42 +0900

    [feat/#26] MyTicketViewModel 로직 추가

--- app/src/main/java/com/sopt/korailtalk/presentation/ui/myticket/MyTicketScreen.kt
+++ app/src/main/java/com/sopt/korailtalk/presentation/ui/myticket/MyTicketScreen.kt
@@ -18,6 +18,7 @@ import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -50,6 +51,8 @@ fun MyTicketScreen(
     var showToast by remember { mutableStateOf(false) }
     val scrollState = rememberScrollState()
 
+    val ticketData by viewModel.ticketData.collectAsState()
+
     Box(
         modifier = Modifier
             .fillMaxSize()
@@ -70,7 +73,7 @@ fun MyTicketScreen(
                     .height(8.dp)
             )
             MyTicket(
-                ticketData = viewModel.ticketDummy,
+                ticketData = ticketData,
                 modifier = Modifier
                     .padding(horizontal = 8.dp)
             )
--- app/src/main/java/com/sopt/korailtalk/presentation/ui/myticket/MyTicketViewModel.kt
+++ app/src/main/java/com/sopt/korailtalk/presentation/ui/myticket/MyTicketViewModel.kt
@@ -1,26 +1,69 @@
 package com.sopt.korailtalk.presentation.ui.myticket
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.sopt.korailtalk.data.remote.model.response.TicketData
 import com.sopt.korailtalk.domain.type.MyTicketTabType
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 import javax.inject.Inject
 
 @HiltViewModel
 class MyTicketViewModel @Inject constructor(
+  //  private val ticketRepository: TicketRepository
 ) : ViewModel()  {
     val myTicketTabText = MyTicketTabType.entries.toTypedArray()
 
-    val ticketDummy = TicketData(
-        departurePlace = "서울",
-        arrivalPlace = "부산",
-        date = "2024년 10월 30일 (수)",
-        trainName = "KTX 001",
-        departureTime = "09:30",
-        arrivalTime = "12:45",
-        seatName = "16A",
-        ticketPrice = 50000,
-        limitPaymentTime = "2024-11-30T18:00:00",
-        coachesNumber = 4
+    private var _myTicketState = MutableStateFlow<MyTicketState>(MyTicketState.Idle)
+    val myTicketState: StateFlow<MyTicketState> = _myTicketState
+
+    private val _ticketData = MutableStateFlow<TicketData>(
+        TicketData(
+            departurePlace = "",
+            arrivalPlace = "",
+            date = "",
+            trainName = "",
+            departureTime = "",
+            arrivalTime = "",
+            seatName = "",
+            ticketPrice = 0,
+            limitPaymentTime = "",
+            coachesNumber = 0
+        )
     )
+    val ticketData: StateFlow<TicketData> get() = _ticketData
+
+    fun getTicketInformation(userId: Long, ticketId: Long) {
+        _myTicketState.value = MyTicketState.Loading
+        viewModelScope.launch {
+            val result = ticketRepository.getTicketInformation(userId = userId, ticketId = ticketId)
+            Log.d("result",result.toString())
+            result.fold(
+                onSuccess = { ticket ->
+                    val formattedTicket = ticket.copy(
+                        limitPaymentTime = formatDate(ticket.limitPaymentTime)
+                    )
+                    _ticketData.value = formattedTicket
+                    _myTicketState.value = MyTicketState.Success(ticket)
+                    Log.d("MyTicketViewModel", "${_myTicketState.value}")
+                },
+                onFailure = { throwable ->
+                    _myTicketState.value = MyTicketState.Failure(throwable.message ?: "알 수 없는 오류")
+                    Log.e("MyTicketViewModel", "로드 실패: ${_myTicketState.value}")
+                }
+            )
+        }
+    }
+
+    private fun formatDate(time: String): String{
+        val inputFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
+        val outputFormatter = DateTimeFormatter.ofPattern("HH시 mm분")
+        val parsedDate = LocalDateTime.parse(time, inputFormatter)
+        return parsedDate.format(outputFormatter)
+    }
 }
